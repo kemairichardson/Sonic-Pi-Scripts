863e930600da30510c1cd1db290aae6599b084c3
kemairichardson <kemai_richardson@davincischools.org>
2018-02-15 13:39:59 -0800

    Add files via upload

---
+++ putp.rb
@@ -0,0 +1,59 @@
+#Pick Up the Phone
+use_bpm 120
+live_loop :putp do
+  play :D4
+  play :G4
+  play :Bb4
+  sleep 1
+  play :r
+  sleep 1
+  play :r
+  play :A4
+  play :C5
+  sleep 0.5
+  play :F4
+  play :B4
+  play :D5
+  sleep 0.5
+  play :G
+  play :C5
+  play :Eb5
+  sleep 1
+  play :r
+  sleep 1
+  play :Eb4
+  play :G4
+  play :Bb4
+  sleep 1
+  play :r
+  sleep 1
+  play :D4
+  play :Fs4
+  play :A4
+  play :C5
+  sleep 1
+  play :r
+  sleep 1
+  sleep 0.5
+  play :F4
+  play :A4
+  play :D5
+  sleep 1
+  play :r
+  sleep 1
+  play :D4
+  play :Fs4
+  play :A4
+  sleep 1.5
+end
+
+
+
+
+
+
+
+
+
+
+
